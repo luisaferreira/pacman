3544e74f503fc93f6de2919b444c3ee7b45e0c0f
Luisa Ferreira <luisaaferreira23@gmail.com>
2023-11-14 23:48:40 +0000

    Refatoração do código

--- main.cpp
+++ main.cpp
@@ -8,16 +8,20 @@
 #include <iostream>
 #include <vector>
 
+string mensagemFinal;
 bool gameOver;
-int x, y, p2x, p2y;
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-eDirection dir, p2dir;
-int pontuacao = 0;
-AnalogIn VRx(PA_0);
-AnalogIn VRy(PA_1);
-AnalogIn Player2VRx(PC_1);
-AnalogIn Player2VRy(PC_0);
+enum eDirecao { LEFT, RIGHT, UP, DOWN };
 DigitalOut V5(PF_1);
+
+struct Jogador{
+    int x, y;
+    AnalogIn VRx, VRy;
+    int pontuacao;
+    eDirecao dir;
+};
+
+Jogador jogador1 = { 14, 4, PA_0, PA_1, 0, DOWN };
+Jogador jogador2 = { 14, 1, PC_1, PC_0, 0, RIGHT };
 
 
 char mapa[9][30] = {
@@ -29,23 +33,19 @@ char mapa[9][30] = {
 
 void Iniciar() {
   gameOver = false;
-  x = 15;
-  y = 4;
-  p2x = 15;
-  p2y = 1;
 }
 
 void RenderizarMapa() {
   printf("\033[2J\033[H");
-  printf("Pontuação: %d\n\n", pontuacao);
+  printf("Pontuação: %d\n\n", jogador1.pontuacao);
   for (int i = 0; i < 9; i++) {
     for (int j = 0; j < 30; j++) {
-      if (i == y && j == x) {
+      if (i == jogador1.y && j == jogador1.x) {
         cout << "\033[1;33m" << '>' << "\033[0m";
         continue;
       }
 
-      if(i == p2y && j == p2x) {
+      if(i == jogador2.y && j == jogador2.x) {
           cout << "\033[1;32m" << '>' << "\033[0m";
           continue;
       }
@@ -68,115 +68,100 @@ void RenderizarMapa() {
 }
 
 void Input() {
-  if (VRx.read() <= 0.33) {
-    dir = LEFT;
-  } else if (VRx.read() >= 0.66) {
-    dir = RIGHT;
-  } else if (VRy.read() <= 0.33) {
-    dir = UP;
-  } else if (VRy.read() >= 0.66) {
-    dir = DOWN;
+  if (jogador1.VRx.read() <= 0.33) {
+    jogador1.dir = LEFT;
+  } else if (jogador1.VRx.read() >= 0.66) {
+    jogador1.dir = RIGHT;
+  } else if (jogador1.VRy.read() <= 0.33) {
+    jogador1.dir = UP;
+  } else if (jogador1.VRy.read() >= 0.66) {
+    jogador1.dir = DOWN;
   }
 }
 
  void Input2() {
-   if (Player2VRx.read() <= 0.33) {
-     p2dir = LEFT;
-   } else if (Player2VRx.read() >= 0.66) {
-     p2dir = RIGHT;
-   } else if (Player2VRy.read() <= 0.33) {
-     p2dir = UP;
-   } else if (Player2VRy.read() >= 0.66) {
-     p2dir = DOWN;
+   if (jogador2.VRx.read() <= 0.33) {
+     jogador2.dir = LEFT;
+   } else if (jogador2.VRx.read() >= 0.66) {
+     jogador2.dir = RIGHT;
+   } else if (jogador2.VRy.read() <= 0.33) {
+     jogador2.dir = UP;
+   } else if (jogador2.VRy.read() >= 0.66) {
+     jogador2.dir = DOWN;
    }
 }
 
 void Logic2() {
-  if (p2dir == UP && mapa[p2y - 1][p2x] == '#')
+  if (jogador2.dir == UP && mapa[jogador2.y - 1][jogador2.x] == '#')
     return;
 
-  if (p2dir == DOWN && mapa[p2y + 1][p2x] == '#')
+  if (jogador2.dir == DOWN && mapa[jogador2.y + 1][jogador2.x] == '#')
     return;
 
-  if (p2dir == RIGHT && mapa[p2y][p2x + 1] == '#')
+  if (jogador2.dir == RIGHT && mapa[jogador2.y][jogador2.x + 1] == '#')
     return;
 
-  if (p2dir == LEFT && mapa[p2y][p2x - 1] == '#')
+  if (jogador2.dir == LEFT && mapa[jogador2.y][jogador2.x - 1] == '#')
     return;
 
   
   
-  switch (p2dir) {
+  switch (jogador2.dir) {
   case UP:
-    p2y--;
+    jogador2.y--;
     break;
   case RIGHT:
-    p2x++;
+    jogador2.x++;
     break;
   case LEFT:
-    p2x--;
+    jogador2.x--;
     break;
   case DOWN:
-    p2y++;
+    jogador2.y++;
     break;
   default:
     break;
   }
 
-    if (p2y == y && p2x == x) {
+  if (jogador2.y == jogador1.y && jogador2.x == jogador1.x) {
     gameOver = true;
+    mensagemFinal = "Jogador 2 venceu";
   }
-
-  // if (x > largura || x < 0 || y > altura || y < 0)
-  //     gameOver = true;
-
-  //   if (x == pontoX && y == pontoY) {
-  //     pontoX = 2;
-  //     pontoY = rand() % altura;
 }
 
 void Logic() {
-  if (dir == UP && mapa[y - 1][x] == '#')
+  if (jogador1.dir == UP && mapa[jogador1.y - 1][jogador1.x] == '#')
     return;
 
-  if (dir == DOWN && mapa[y + 1][x] == '#')
+  if (jogador1.dir == DOWN && mapa[jogador1.y + 1][jogador1.x] == '#')
     return;
 
-  if (dir == RIGHT && mapa[y][x + 1] == '#')
+  if (jogador1.dir == RIGHT && mapa[jogador1.y][jogador1.x + 1] == '#')
     return;
 
-  if (dir == LEFT && mapa[y][x - 1] == '#')
+  if (jogador1.dir == LEFT && mapa[jogador1.y][jogador1.x - 1] == '#')
     return;
 
-  
-  
-  switch (dir) {
+  switch (jogador1.dir) {
   case UP:
-    y--;
+    jogador1.y--;
     break;
   case RIGHT:
-    x++;
+    jogador1.x++;
     break;
   case LEFT:
-    x--;
+    jogador1.x--;
     break;
   case DOWN:
-    y++;
+    jogador1.y++;
     break;
   default:
     break;
   }
-  if (mapa[y][x] == '*') {
-    pontuacao += 10;
-    mapa[y][x] = ' ';
+  if (mapa[jogador1.y][jogador1.x] == '*') {
+    jogador1.pontuacao += 10;
+    mapa[jogador1.y][jogador1.x] = ' ';
   }
-
-  // if (x > largura || x < 0 || y > altura || y < 0)
-  //     gameOver = true;
-
-  //   if (x == pontoX && y == pontoY) {
-  //     pontoX = 2;
-  //     pontoY = rand() % altura;
 }
 
 int main() {
@@ -192,5 +177,7 @@ int main() {
     Logic();
     Logic2();
   }
+
+  cout << mensagemFinal;
   return 0;
 }
